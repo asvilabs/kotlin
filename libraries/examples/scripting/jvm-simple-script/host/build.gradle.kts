b3f7d8e9494bd528ecd41bb3ae51bfec44501082
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2018-08-31 13:32:35 +0200

    [minor] fix script example dependency

--- libraries/examples/scripting/jvm-simple-script/host/build.gradle.kts
+++ libraries/examples/scripting/jvm-simple-script/host/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     compile(project(":kotlin-scripting-jvm-host"))
     compile(project(":kotlin-script-util"))
     testRuntimeOnly(projectRuntimeJar(":kotlin-compiler"))
+    testRuntimeOnly(projectRuntimeJar(":kotlin-reflect"))
     testCompile(commonDep("junit"))
 }
 
